25661b21b075a5e21fb8a1de2a064f1f93984aa5
ArthurAnjos31 <136720258+ArthurAnjos31@users.noreply.github.com>
2024-10-28 11:00:56 -0300

    Add files via upload

--- AAaa.js
+++ AAaa.js
@@ -1,3 +1,6 @@
 alert("Cuidado, o tigrin vai ti pega.   " );
-let numeroSecreto = 7.4;
+let numeroSecreto = 7,4;
+let chute;          
 prompt(" Escolha um numero entre 1,0 e 100,0       ");
+if(numeroSecreto > chute){ alert("O numeroSecreto é maior que ${chute}}")   }
+else{alert("O numeroSecreto é menor que ${chute}}")}
